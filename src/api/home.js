80591010ad65e5a62955f909b381f1ad801e76b1
mjzhang1993 <mingjiazhang@yufuid.com>
2018-08-16 11:46:00 +0800

    sss

--- src/api/home.js
+++ src/api/home.js
@@ -1,20 +1,19 @@
 /*
    home 的网络请求部分
 */
-import { get, post } from './request';
+import {get, post} from './request';
 
 export function obtainMemberList() {
-   return get('/list').then(res => res.data);
+  return get('/list').then((res) => res.data);
 }
 
 export function postNewMember(newMember) {
-   if (!newMember.name) {
-      return Promise.reject('name is wrong');
-   }
-   return post('/list', newMember).then(res => res.data);
+  if (!newMember.name) {
+    return Promise.reject('name is wrong');
+  }
+  return post('/list', newMember).then((res) => res.data);
 }
 
-export function test(){
-    
-    return "th isi test"
+export function test() {
+  return '  th isi test';
 }
